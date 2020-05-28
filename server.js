7f925c8ac7e78e62c7918f7b9bde9a378f4eb2c0
Mauricio Cartagena <mc973220@gmail.com>
2020-05-27 20:24:19 -0400

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -26,44 +26,44 @@ var connection = mysql.createPool({
 });
 // Route
 app.get("/", (req, res) => {
-  res.send("rerror raro ");
+  res.send("Iniciando API");
 });
-app.post(
-  "/usuarios/:nombre/:ape_paterno/:ape_materno/:ci/:genero/:direccion",
-  (req, res) => {
-    let nombre = req.params.nombre;
-    let apellido_paterno = req.params.ape_paterno;
-    let apellido_materno = req.params.ape_materno;
-    let ci = req.params.ci;
-    let genero = req.params.genero;
-    let direccion = req.params.direccion;
-    connection.query(
-      "INSERT INTO `usuarios` (`nombre`,`apellido_paterno`,`apellido_materno`,`ci`,`genero`,`direccion`)VALUES ('" +
-        nombre +
-        "','" +
-        apellido_paterno +
-        "','" +
-        apellido_materno +
-        "','" +
-        ci +
-        "','" +
-        genero +
-        "','" +
-        direccion +
-        "');",
-      function (err, data) {
-        err ? res.send(err) : res.json({ usuarios: data });
-      }
-    );
-  }
-);
+// app.post(
+//   "/usuarios/:nombre/:ape_paterno/:ape_materno/:ci/:genero/:direccion",
+//   (req, res) => {
+//     let nombre = req.params.nombre;
+//     let apellido_paterno = req.params.ape_paterno;
+//     let apellido_materno = req.params.ape_materno;
+//     let ci = req.params.ci;
+//     let genero = req.params.genero;
+//     let direccion = req.params.direccion;
+//     connection.query(
+//       "INSERT INTO `usuarios` (`nombre`,`apellido_paterno`,`apellido_materno`,`ci`,`genero`,`direccion`)VALUES ('" +
+//         nombre +
+//         "','" +
+//         apellido_paterno +
+//         "','" +
+//         apellido_materno +
+//         "','" +
+//         ci +
+//         "','" +
+//         genero +
+//         "','" +
+//         direccion +
+//         "');",
+//       function (err, data) {
+//         err ? res.send(err) : res.json({ usuarios: data });
+//       }
+//     );
+//   }
+// );
 // app.get("/usuarios", function (req, res) {
 //   connection.query("Select * from usuarios", function (err, data) {
 //     err ? res.send(err) : res.json({ usuarios: data });
 //   });
 // });
-app.get("/usuarios", (req, res) => {
-  const sql = "SELECT * FROM usuarios";
+app.get("/questions", (req, res) => {
+  const sql = "SELECT nombre,id_pregunta FROM `pregunta_formas` ";
 
   connection.query(sql, (error, results) => {
     if (error) throw error;
@@ -74,63 +74,60 @@ app.get("/usuarios", (req, res) => {
     }
   });
 });
-app.get("/usuarios/:id", function (req, res) {
-  let id = req.params.id;
-  connection.query("Select * from usuarios WHERE id_usuario =" + id, function (
-    err,
-    data
-  ) {
-    err ? res.send(err) : res.json({ usuarios: data });
-  });
-});
-app.delete("/usuarios/delete/:id", function (req, res) {
-  let id = req.params.id;
-  connection.query("DELETE FROM usuarios WHERE id_usuario=" + id, function (
-    err,
-    data
-  ) {
-    err ? res.send(err) : res.send("success");
-  });
-});
-
-app.put(
-  "/usuarios/update/:id/:nombre/:ape_paterno/:ape_materno/:ci/:genero/:direccion",
-  function (req, res) {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-    let apellido_paterno = req.params.ape_paterno;
-    let apellido_materno = req.params.ape_materno;
-    let ci = req.params.ci;
-    let genero = req.params.genero;
-    let direccion = req.params.direccion;
-
-    connection.query(
-      "UPDATE usuarios SET nombre = '" +
-        nombre +
-        "', apellido_paterno = '" +
-        apellido_paterno +
-        "', apellido_materno= '" +
-        apellido_materno +
-        "', ci='" +
-        ci +
-        "', genero='" +
-        genero +
-        "', direccion='" +
-        direccion +
-        "' WHERE id_usuario='" +
-        id +
-        "'  LIMIT 1",
-      function (err, data) {
-        err ? res.send(err) : res.send("success");
-      }
-    );
-  }
-);
-
-// // Check connect
-// connection.connect((error) => {
-//   if (error) throw error;
-//   console.log("Database server running!");
+// app.get("/usuarios/:id", function (req, res) {
+//   let id = req.params.id;
+//   connection.query("Select * from usuarios WHERE id_usuario =" + id, function (
+//     err,
+//     data
+//   ) {
+//     err ? res.send(err) : res.json({ usuarios: data });
+//   });
+// });
+// app.delete("/usuarios/delete/:id", function (req, res) {
+//   let id = req.params.id;
+//   connection.query("DELETE FROM usuarios WHERE id_usuario=" + id, function (
+//     err,
+//     data
+//   ) {
+//     err ? res.send(err) : res.send("success");
+//   });
 // });
 
+// app.put(
+//   "/usuarios/update/:id/:nombre/:ape_paterno/:ape_materno/:ci/:genero/:direccion",
+//   function (req, res) {
+//     let id = req.params.id;
+//     let nombre = req.params.nombre;
+//     let apellido_paterno = req.params.ape_paterno;
+//     let apellido_materno = req.params.ape_materno;
+//     let ci = req.params.ci;
+//     let genero = req.params.genero;
+//     let direccion = req.params.direccion;
+
+//     connection.query(
+//       "UPDATE usuarios SET nombre = '" +
+//         nombre +
+//         "', apellido_paterno = '" +
+//         apellido_paterno +
+//         "', apellido_materno= '" +
+//         apellido_materno +
+//         "', ci='" +
+//         ci +
+//         "', genero='" +
+//         genero +
+//         "', direccion='" +
+//         direccion +
+//         "' WHERE id_usuario='" +
+//         id +
+//         "'  LIMIT 1",
+//       function (err, data) {
+//         err ? res.send(err) : res.send("success");
+//       }
+//     );
+//   }
+// );
+connection.getConnection((error) => {
+  if (error) throw error;
+  console.log("Database server running!");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
